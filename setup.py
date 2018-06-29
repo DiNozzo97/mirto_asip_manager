4643e639c933f374728c15456d9507a9f6f82076
Adam Jarzebak <adam@jarzeabak.eu>
2018-06-30 00:56:37 +0100

    Add files, add setup. WIP for structure

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mirto_asip",
+    version="0.0.1",
+    author="Adam Jarzebak",
+    author_email="adam@jarzebak.eu",
+    description="Serial manager for mirto robot services",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/example-project",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
