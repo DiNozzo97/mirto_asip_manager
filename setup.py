2fcf3f61969a166664ad123e92dfe93124c32f6a
Adam Jarzebak <adam@jarzeabak.eu>
2018-06-30 01:06:57 +0100

    Finish setup file

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="mirto_asip",
-    version="0.0.1",
+    name="mirto_asip_manager",
+    version="1.0",
     author="Adam Jarzebak",
     author_email="adam@jarzebak.eu",
     description="Serial manager for mirto robot services",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/example-project",
+    url="https://github.com/jarzab3/mirto_asip_manager",
     packages=setuptools.find_packages(),
     classifiers=(
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6.4",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Operating System :: Ubuntu Mate - ARM",
     ),
 )
