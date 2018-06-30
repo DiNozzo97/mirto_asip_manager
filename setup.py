14a015249e0d582c6858f912203f9a66ddba1118
Adam Jarzebak <adam@jarzeabak.eu>
2018-06-30 01:39:10 +0100

    Restructure project to add modules

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ setuptools.setup(
     version="1.0",
     author="Adam Jarzebak",
     author_email="adam@jarzebak.eu",
-    description="Serial manager for mirto robot services",
+    description="Serial manager for Mirto robot services",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/jarzab3/mirto_asip_manager",
-    packages=setuptools.find_packages(),
+    packages=['mirto_asip_manager'],
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
